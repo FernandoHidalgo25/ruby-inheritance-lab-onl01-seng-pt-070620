fb850dd775c08b4c41fff8052f36e92e020d835b
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-06 06:38:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge=Array.new
+  end
+
+  def learn(neww)
+    @knowledge<<neww
+  end
+  def knowledge
+    @knowledge
+  end
 end
